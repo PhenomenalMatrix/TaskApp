d0e15291fcfce9bc2d76d41f2d6d2aefa79458b2
PhenomenalMatrix <mrflaitx@gmail.com>
2020-05-03 16:24:01 +0600

    Initial commit

---
+++ app/src/main/java/com/example/taskapp/FormActivity.java
@@ -0,0 +1,45 @@
+package com.example.taskapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import com.example.taskapp.models.Task;
+
+public class FormActivity extends AppCompatActivity {
+
+    private EditText editTitle;
+    private EditText editDesc;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form);
+        if (getSupportActionBar() !=null){
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setTitle("Новая Задача");
+        }
+        editTitle = findViewById(R.id.editTitle);
+        editDesc = findViewById(R.id.editDes);
+    }
+
+    public void onClick(View view) {
+        String title = editTitle.getText().toString().trim();
+        String desc = editDesc.getText().toString().trim();
+        Task task = new Task(title, desc);
+        Intent intent = new Intent();
+        intent.putExtra("task", task);
+        setResult(RESULT_OK, intent);
+        finish();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("des","des");
+    }
+}
